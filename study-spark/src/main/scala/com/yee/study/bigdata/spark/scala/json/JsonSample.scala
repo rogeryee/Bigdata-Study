5b612532995d83402f494d92d074a8972c890b2e
yish <yish@tpl.cntaiping.com>
2021-12-09 16:03:08 +0800

    update

--- study-spark/src/main/scala/com/yee/study/bigdata/spark/scala/json/JsonSample.scala
+++ study-spark/src/main/scala/com/yee/study/bigdata/spark/scala/json/JsonSample.scala
@@ -1,6 +1,9 @@
 package com.yee.study.bigdata.spark.scala.json
 
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{col, collect_list, struct, to_json}
+
 
 /**
  *
@@ -9,14 +12,26 @@ import org.apache.spark.sql.DataFrame
 object JsonSample {
 
   def main(args: Array[String]): Unit = {
-    import org.apache.spark.sql.SparkSession
+
     val spark = SparkSession.builder()
       .master("local[*]").appName("test")
       .getOrCreate();
-    //提供隐式转换功能，比如将 Rdd 转为 dataframe
-    import spark.implicits._
 
-    val df: DataFrame = spark.sparkContext.parallelize(Array(("abc", 2), ("efg", 4))).toDF()
+    import spark.implicits._
+    val df: DataFrame = spark.sparkContext.parallelize(Array(
+      ("hol_001", "ins_001", "张三", "F"),
+      ("hol_001", "ins_002", "李四", "F"),
+      ("hol_002", "ins_003", "王五", "M"),
+      ("hol_003", "ins_004", "赵六", "M"))).toDF("hol_id", "ins_id", "ins_name", "ins_gender")
     df.show()
+
+    val jsonStr: String = df.toJSON.collectAsList.toString
+    println(jsonStr)
+
+//    val df2: DataFrame = df.groupBy("hol_id").agg(collect_list(struct("insured_id", col("ins_id"), "ins_name", "ins_gender"))).alias("ins_body")
+    val df2: DataFrame = df.groupBy("hol_id").agg(collect_list(struct("ins_id", "ins_name", "ins_gender"))).alias("ins_body")
+    df2.show()
+
+//    println(df2.toJSON.collectAsList().toString)
   }
 }
