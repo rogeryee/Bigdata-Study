994cec849199960958c52555ec22e295b980f38c
yish <yish@tpl.cntaiping.com>
2022-02-09 15:38:56 +0800

    add wordcount sample

---
+++ study-flink/src/main/java/com/yee/study/bigdata/flink/wordcount/StreamingWordCount.java
@@ -0,0 +1,41 @@
+package com.yee.study.bigdata.flink.wordcount;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * nc -lk 6789
+ *
+ * @author Roger.Yi
+ */
+public class StreamingWordCount {
+
+    public static void main(String[] args) throws Exception {
+        // 获取执行环境对象 StreamExecutionEnvironment
+        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+        executionEnvironment.setParallelism(1);
+
+        // 加载数据源获取数据抽象对象
+        DataStreamSource<String> sourceDataStream = executionEnvironment.socketTextStream("localhost", 6789);
+
+        // 执行逻辑处理
+        SingleOutputStreamOperator<Tuple2<String, Integer>> wordAndOneDS = sourceDataStream.flatMap((FlatMapFunction<String, Tuple2<String, Integer>>) (line, collector) -> {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                collector.collect(Tuple2.of(word, 1));
+            }
+        }).setParallelism(3);
+
+        // 分组聚合
+        SingleOutputStreamOperator<Tuple2<String, Integer>> resultDS = wordAndOneDS.keyBy("0").sum(1).setParallelism(1);
+
+        // 输出结果
+        resultDS.print().setParallelism(5);
+
+        // 提交执行
+        executionEnvironment.execute("StreamingWordCount running.");
+    }
+}
