4a217822dad061811e97a1b5b14fb7c3369c6871
yish <yish@tpl.cntaiping.com>
2022-02-09 18:25:28 +0800

    add sample

---
+++ study-flink/src/main/java/com/yee/study/bigdata/flink/source/userdefine/SourceWithParallel.java
@@ -0,0 +1,69 @@
+package com.yee.study.bigdata.flink.source.userdefine;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+/**
+ * 自定义的 带并行度 的数据源
+ * Source Operator 的 Task 只有一个！
+ * 1、单并行度： MySource implements SourceFunction<T>
+ * 2、多并行度： MySource implements ParallelSourceFunction<T>
+ *
+ * @author Roger.Yi
+ */
+public class SourceWithParallel {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 这个数据源：有节奏的每隔 1s 输出一个顺序递增的自然数
+        DataStreamSource<Long> numberDS = executionEnvironment.addSource(new ParallelSource()).setParallelism(5);
+
+        // 先map一下，再filter一下
+        SingleOutputStreamOperator<Long> resultDS = numberDS.map(new MapFunction<Long, Long>() {
+            @Override
+            public Long map(Long value) throws Exception {
+                System.out.println("接收到了数据：" + value);
+                return value;
+            }
+        }).filter(new FilterFunction<Long>() {
+            @Override
+            public boolean filter(Long value) throws Exception {
+                return value % 2 == 0;
+            }
+        }).setParallelism(2);
+
+        // 输出
+        resultDS.print().setParallelism(2);
+
+        executionEnvironment.execute("SourceWithParallel");
+    }
+}
+
+/**
+ * 自定义一个带并行度的 Source，每秒输出一个递增的自然数
+ */
+class ParallelSource implements ParallelSourceFunction<Long> {
+
+    private boolean running = true;
+
+    private long number = 1l;
+
+    @Override
+    public void run(SourceContext<Long> ctx) throws Exception {
+        while (running) {
+            ctx.collect(number++);
+            Thread.sleep(1000);
+        }
+    }
+
+    @Override
+    public void cancel() {
+        running = false;
+    }
+}
