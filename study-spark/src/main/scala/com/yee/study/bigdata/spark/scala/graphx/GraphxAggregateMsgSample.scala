b5f89e2fa38c2da8ea1b5d5bb9f29fa3f265ef29
yish <yish@tpl.cntaiping.com>
2023-01-18 09:52:06 +0800

    增加 GraphX 示例

--- study-spark/src/main/scala/com/yee/study/bigdata/spark/scala/graphx/GraphxAggregateMsgSample.scala
+++ study-spark/src/main/scala/com/yee/study/bigdata/spark/scala/graphx/GraphxAggregateMsgSample.scala
@@ -5,7 +5,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 /**
- * 使用GraphX计算粉丝平均年龄 (基于 aggregateMessage 方法)
+ * 使用GraphX计算粉丝平均年龄和最大年龄 (基于 aggregateMessage 方法)
  *
  * aggregateMessages 在图结构中实现了一个基本的map/reduce编程模型。
  * sendMsg 是map过程，每条边向其src或dst发送一个消息。其输入参数为EdgeContext类型。EdgeContext类型比Triplet类型多了sendToSrc和sendToDst两个方法，用于发送消息。
@@ -42,8 +42,17 @@ object GraphxAggregateMsgSample {
       .map(row => Edge(row(0).toLong, row(1).toLong, 1))
 
     val graph: Graph[(String, Long), Long] = Graph(usersRDD, followersRDD)
-    //    graph.triplets.collect.foreach(println)
+    graph.triplets.collect.foreach(println)
 
+    // 计算粉丝平均年龄
+    avgAgeOfFollowers(graph)
+
+    // 计算粉丝最大年龄
+    maxAgeOfFollowers(graph)
+  }
+
+  // 计算粉丝平均年龄
+  def avgAgeOfFollowers(graph: Graph[(String, Long), Long]): Unit = {
     // 计算粉丝数量以及年龄和
     // def aggregateMessages[A: ClassTag](
     //      sendMsg: EdgeContext[VD, ED, A] => Unit,
@@ -61,11 +70,40 @@ object GraphxAggregateMsgSample {
 
     // 计算平均年龄
     val avgAgeOfFollowers: VertexRDD[Double] =
-      followers.mapValues((id, value) => value match { case (count, totalAge) => totalAge / count })
+      followers.mapValues((id, value) => value match {
+        case (count, totalAge) => totalAge / count
+      })
 
     // (2,27.0)
     // (1,21.0)
     // (3,35.0)
     avgAgeOfFollowers.collect.foreach(println)
+  }
+
+  // 计算粉丝最大年龄
+  def maxAgeOfFollowers(graph: Graph[(String, Long), Long]): Unit = {
+    // 计算粉丝最大年龄
+    // def aggregateMessages[A: ClassTag](
+    //      sendMsg: EdgeContext[VD, ED, A] => Unit,
+    //      mergeMsg: (A, A) => A,
+    //      tripletFields: TripletFields = TripletFields.All)
+    val followers: VertexRDD[Long] = graph.aggregateMessages[Long](
+      triplet => {
+        // 发送年龄到被关注者
+        triplet.sendToDst(triplet.srcAttr._2)
+      },
+
+      // 比较年龄，取较大值
+      (a, b) => if (a > b) a else b
+    )
+
+    // 计算最大年龄
+    val maxAgeOfFollowers: VertexRDD[Long] =
+      followers.mapValues(x => x)
+
+    // (2,30)
+    // (1,25)
+    // (3,35)
+    maxAgeOfFollowers.collect.foreach(println)
   }
 }
