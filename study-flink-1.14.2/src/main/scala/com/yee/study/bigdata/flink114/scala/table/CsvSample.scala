f534f71d87d6958339168e3d041a11feb5a2cb0f
yish <yish@tpl.cntaiping.com>
2023-02-11 10:22:07 +0800

    refactor flink table sample with scala

---
+++ study-flink-1.14.2/src/main/scala/com/yee/study/bigdata/flink114/scala/table/CsvSample.scala
@@ -0,0 +1,72 @@
+package com.yee.study.bigdata.flink114.scala.table
+
+import org.apache.flink.table.api.{DataTypes, EnvironmentSettings, FormatDescriptor, Schema, TableDescriptor, TableEnvironment}
+
+/**
+ * 基于 Flink Table，读取csv内容并输出到新的csv的示例
+ *
+ * @author Roger.Yi
+ */
+object CsvSample {
+
+  def main(args: Array[String]): Unit = {
+    val envSettings = EnvironmentSettings.newInstance().inBatchMode().build()
+    val tabEnv = TableEnvironment.create(envSettings)
+
+    // Source
+    tabEnv.createTemporaryTable(
+      "csv_sample",
+      TableDescriptor
+        .forConnector("filesystem")
+        .schema(
+          Schema.newBuilder()
+            .column("id", DataTypes.BIGINT())
+            .column("name", DataTypes.STRING())
+            .column("age", DataTypes.INT())
+            .build())
+        .option("path", "file:///Users/cntp/MyWork/yee/bigdata-study/study-flink-1.14.2/data/table_csv_sample.csv")
+        .format(
+          FormatDescriptor.forFormat("csv")
+            .option("field-delimiter", ",")
+            .build())
+        .build());
+
+    val csv_sample = tabEnv.sqlQuery(
+      """
+        |select
+        |   id
+        | , name
+        | , age
+        |from csv_sample
+        |where age > 25
+        |""".stripMargin)
+
+    /**
+     * (
+     * `id` BIGINT,
+     * `name` STRING,
+     * `age` INT)
+     */
+    csv_sample.printSchema()
+
+    // Sink
+    tabEnv.createTemporaryTable(
+      "csv_sample_sink",
+      TableDescriptor
+        .forConnector("print")
+        .schema(
+          Schema.newBuilder()
+            .column("id", DataTypes.BIGINT())
+            .column("name", DataTypes.STRING())
+            .column("age", DataTypes.INT())
+            .build())
+        .build());
+
+    /**
+     * 3> +I[103, wangwu, 28]
+     * 7> +I[104, laoyou, 30]
+     * 1> +I[105, laowang, 35]
+     */
+    csv_sample.executeInsert("csv_sample_sink");
+  }
+}
---
+++ study-flink-1.14.2/src/main/scala/com/yee/study/bigdata/flink114/scala/table/DataStreamSample.scala
@@ -0,0 +1,40 @@
+package com.yee.study.bigdata.flink114.scala.table
+
+import org.apache.flink.api.scala.createTypeInformation
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
+import org.apache.flink.util.Collector
+
+/**
+ * DataStream 和 Table 配合使用的示例1
+ * DataStream 获取Socket的数据
+ *
+ * nc -lk 6789
+ *
+ * @author Roger.Yi
+ */
+object DataStreamSample {
+
+  def main(args: Array[String]): Unit = {
+    val sEnv = StreamExecutionEnvironment.getExecutionEnvironment
+    val tEnv = StreamTableEnvironment.create(sEnv)
+
+    // Source
+    val dataStream = sEnv.socketTextStream("localhost", 6789)
+
+    // Operation
+    val wordCntDs = dataStream.flatMap((str: String, out: Collector[(String, Int)]) => {
+      str.split(",").foreach(s => out.collect(Tuple2(s, 1)))
+    })
+
+    // Convert DataStream to Table
+    val wordTable = tEnv.fromDataStream(wordCntDs).as("word", "state");
+    tEnv.createTemporaryView("wordTable", wordTable);
+    val resultTable = tEnv.sqlQuery("select word, count(1) as cnt from wordTable group by word");
+
+    // Sink
+    resultTable.execute().print();
+    sEnv.execute("");
+  }
+}
+
